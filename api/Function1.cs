2703980d8cab7dcef28eef4fe710dba3fb458d99
Nao Animal Hospital <stardust@af.wakwak.com>
2024-10-12 15:16:53 +0900

    エラーなおしたはず

--- api/Function1.cs
+++ api/Function1.cs
@@ -19,15 +19,8 @@ namespace api
         {
             log.LogInformation("C# HTTP trigger function processed a request.");
 
-            string name = req.Query["name"];
 
-            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
-            dynamic data = JsonConvert.DeserializeObject(requestBody);
-            name = name ?? data?.name;
-
-            string responseMessage = string.IsNullOrEmpty(name)
-                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
-                : $"Hello, {name}. This HTTP triggered function executed successfully.";
+            string responseMessage = "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
             return new OkObjectResult(responseMessage);
         }
